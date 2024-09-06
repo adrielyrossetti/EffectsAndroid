15b8bacf63da7bcf0ece1b04f1757101d344c1fe
AdriRossetti <adrielyfragarossetti@gmail.com>
2024-09-06 10:00:59 -0300

    transicoes

--- app/src/main/java/com/example/efeitosnoandroid/MainActivity.java
+++ app/src/main/java/com/example/efeitosnoandroid/MainActivity.java
@@ -7,6 +7,7 @@ import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView txtTexto = findViewById(R.id.txtTexto);
-
+        ImageView imgsunflower = findViewById(R.id.imgsunflower);
         Button btnAnimacao = findViewById(R.id.btnAnimacao);
         btnAnimacao.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +31,23 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-        
+        Button btnTransparencia = findViewById(R.id.btnTransparencia);
+        btnTransparencia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgsunflower.setImageAlpha(1);
+            }
+        });
+        Button btnTransicao = findViewById(R.id.btnTransicao);
+        btnTransicao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgsunflower.animate().translationX(-200f).setDuration(500).start();
+            }
+        });
+
+
+
 
 
     }
